5c1b0406565ffbf2b35c2abf6d29435d0ddc64e5
itslearninggermany <45273598+itslearninggermany@users.noreply.github.com>
2019-10-29 14:42:50 +0100

    Update wsse_security.go

--- wsse_security.go
+++ wsse_security.go
@@ -12,32 +12,15 @@ type Security struct {
 	} `xml:"UsernameToken"`
 }
 
-/*
 
- */
-func NewSecurity() *Security {
-	out := new(Security)
-	out.Wsse = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"
-	out.Wsu = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"
-	out.UsernameToken.Password.Type = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText"
-	return out
+type WSSESecurity struct {
+	username string
+	passwort string
 }
 
-/*
-
- */
-func NewSecurityWithCredentials(username string, password string) *Security {
-	out := NewSecurity()
-	out.UsernameToken.Username = username
-	out.UsernameToken.Password.Text = password
-	return out
-}
-
-/*
-
- */
-func (p *Security) SetCredentials(username string, password string) *Security {
-	p.UsernameToken.Username = username
-	p.UsernameToken.Password.Text = password
+func NewWSSESecurity (username, password string) *WSSESecurity{
+	p := new(WSSESecurity)
+	p.username = username
+	p.passwort = password
 	return p
 }
